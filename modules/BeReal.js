a0022e7ac456c2abc0da1f1185a510bb05c8da7f
Amaan Hussain <amaan6307@gmail.com>
2023-04-13 16:52:17 -0500

    added cosmicjs image storing

--- modules/BeReal.js
+++ modules/BeReal.js
@@ -1,6 +1,23 @@
 import { Router } from "express";
 var router = Router();
 
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+import fetch from "node-fetch";
+
+import multer from 'multer'
+const upload = multer({ dest: 'uploads/' });
+import fs from 'fs'
+
+import Cosmic from 'cosmicjs'
+const bucket = Cosmic().bucket({
+    slug: process.env.COSMICBUCKETSLUG,
+    read_key: process.env.COSMICREADKEY,
+    write_key: process.env.COSMICWRITEKEY
+})
+
+
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, get } from "firebase/database"
 
@@ -19,9 +36,25 @@ function setBeReals(uid, value) {
     return set(ref(database, 'users/' + uid + '/bereal'), value)
 }
 
-router.post('/new', function (req, res) {
-    const pic1 = req.body.pic1;
-    const pic2 = req.body.pic2;
+async function uploadToCosmic(files) {
+    var urls = [];
+
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const data = await bucket.media.insertOne({
+            media: {
+                originalname: file.originalname,
+                buffer: fs.createReadStream(file.path)
+            },
+            folder: 'berealimages'
+        });
+        const url = data.media.url;
+        urls.push(url)
+    }
+    return urls;
+}
+
+router.post('/new', upload.array('file', 2), async function (req, res) {
 
     var token = req.headers.authorization;
     if (!token) {
@@ -29,10 +62,10 @@ router.post('/new', function (req, res) {
     }
     token = token.replace("Bearer ", "");
 
-    authorizeToken(token).then((decodedToken) => {
+    authorizeToken(token).then(async (decodedToken) => {
         const uid = decodedToken.uid;
 
-        getBeReals(uid).then((snapshot) => {
+        getBeReals(uid).then(async (snapshot) => {
             var data = snapshot.val();
             if (!data) {
                 data = [];
@@ -42,27 +75,30 @@ router.post('/new', function (req, res) {
             const yyyy = date.getFullYear();
             const mm = String(date.getMonth() + 1).padStart(2, '0');
             const dd = String(date.getDate()).padStart(2, '0');
-            const formattedDate = `${yyyy}-${mm}-${dd}`;
+            const formattedDate = `${yyyy}-${mm} -${dd} `;
 
             for (let i = 0; i < data.length; i++) {
                 const bereal = data[i];
                 if (bereal.date == formattedDate) {
-                    res.send({"error": "bereal already sent today"});
+                    res.send({ "error": "bereal already sent today" });
                     return;
                 }
             }
 
+            const files = req.files;
+            if (!files) {
+                res.send({ "error": "please submit with images" });
+                return;
+            }
+            const urls = await uploadToCosmic(files);
+
             const todaysBereal = {
                 date: formattedDate,
-                pics: [pic1, pic2]
+                pics: urls
             }
-
             data.push(todaysBereal)
-
-            console.log(data)
-
             setBeReals(uid, data).then(() => {
-                res.send({"success": "uploaded bereal"})
+                res.send({ "success": "uploaded bereal" })
             })
         })
     })
@@ -85,17 +121,22 @@ router.post('/delete', function (req, res) {
             const yyyy = date.getFullYear();
             const mm = String(date.getMonth() + 1).padStart(2, '0');
             const dd = String(date.getDate()).padStart(2, '0');
-            const formattedDate = `${yyyy}-${mm}-${dd}`;
-    
+            const formattedDate = `${yyyy} -${mm} -${dd} `;
+
+            var count = 0;
             for (let i = 0; i < data.length; i++) {
                 const bereal = data[i];
                 if (bereal.date == formattedDate) {
                     data.splice(i, 1);
                     setBeReals(uid, data).then(() => {
-                        res.send({"success": "deleted bereal"})
+                        res.send({ "success": "deleted bereal" })
                         return;
                     })
-                }
+                } else { count += 1 }
+            }
+            if (count == data.length) {
+                res.send({ "error": "couldnt delete todays bereal" })
+                return;
             }
         })
     })
